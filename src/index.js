8e4e443b6e5eb4e3390028bd091d7d7db64e73b3
Mike Cooper <mythmon@gmail.com>
2016-07-18 22:00:55 -0700

    redux

--- src/index.js
+++ src/index.js
@@ -1,19 +1,49 @@
 import React, { Component, PropTypes as pt } from 'react';
 import ReactDOM from 'react-dom';
 
+import { createStore } from 'redux';
+import { Provider, connect } from 'react-redux';
+
+const initialState = {
+  names: ['Alice', 'Bob', 'Charlie'],
+};
+
+function reducer(state = initialState, action = {}) {
+  switch (action.type) {
+    default: {
+      return state;
+    }
+  }
+}
+
+const store = createStore(reducer);
+
+@connect(state => state)
 class Main extends Component {
   static propTypes = {
-    target: pt.string.isRequired,
+    names: pt.array.isRequired,
   }
 
   componentWillMount() {
   }
 
   render() {
-    const { target } = this.props;
-    return <h1>Hello {target}!</h1>;
+    const { names } = this.props;
+    return (
+      <div>
+        {names.map(name => (
+          <h1 key={name}>Hello {name}!</h1>
+        ))}
+      </div>
+    );
   }
 }
 
+
 const target = document.querySelector('#main');
-ReactDOM.render(<Main target="World" />, target);
+ReactDOM.render(
+  <Provider store={store}>
+    <Main />
+  </Provider>,
+  target,
+);
